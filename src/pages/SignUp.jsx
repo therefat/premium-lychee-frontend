1554a64c2bf18f912aa02c7d00ecb422dcf73cc8
mdrefathossen <refathossen2001@gmail.com>
2023-11-26 23:58:51 +0600

    some change in layout

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -32,7 +32,7 @@ function SignUp() {
     <Layout>
       <div>
         <div>SignUp</div>
-        <div className=" flex m-9 flex-col justify-center  overflow-hidden">
+        <div className="relative flex  flex-col  justify-center  overflow-hidden">
         <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
             <h1 className="text-3xl font-semibold text-center text-purple-700">SignUp</h1>
             <form className="space-y-4" method="post" onSubmit={(e) => submitForm(e)}>
