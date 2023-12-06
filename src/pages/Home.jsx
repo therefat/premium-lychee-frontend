caa8afac0206064d50e1f959491063fb7dea91b9
mdrefathossen <refathossen2001@gmail.com>
2023-12-06 12:51:37 +0600

    homepage slider added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Layout from '../layout/Layout'
+import slider1 from '../assets/images/sliderImage/slider1.png'
+import slider2 from '../assets/images/sliderImage/slider2.png'
+import slider3 from '../assets/images/sliderImage/slider3.jpg'
 import Slider from 'react-slick'
 
 function Home() {
@@ -7,6 +10,7 @@ function Home() {
     dots : true,
     infinite : true,
     speed: 500,
+    autoplay: true,
     slidesToShow : 1,
     slidesToScroll : 1
   }
@@ -15,14 +19,14 @@ function Home() {
     <Layout>
        <div className="sliders">
        <Slider {...sliderSettings}>
-            <div>
-              <h3>1</h3>
+            <div className=' '>
+              <img src={slider1}  className="h-[300px] w-[100%] "  alt="" />
             </div>
-            <div>
-              <h3>2</h3>
+            <div className=''>
+            <img src={slider2} alt="" className='h-[300px] w-[100%]' />
             </div>
-            <div>
-              <h3>3</h3>
+            <div className=''>
+            <img className='h-[300px] w-[100%]' src={slider3} alt="" />
             </div>
         </Slider>
        </div>
